69c4d1b2e30db65d396515b9da57d86f0f3e411d
Melissa Warrens <melissa.warrens@continuum.be>
2022-10-29 16:42:18 +0200

    more info

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,12 @@
 // Here you can put your custom commands
+
+// TODO: create your own custom command
+
+/**
+ *
+ * Here you can see an example of a custom command
+ *
+ */
+// Cypress.Commands.add('goToPage', (page: string, ...args) => {
+//   cy.visit(page);
+// });
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,1 +1,7 @@
 /// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    goToPage: (value: string) => Chainable<JQuery>;
+  }
+}
